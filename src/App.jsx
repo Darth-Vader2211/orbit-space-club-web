70e1b24ef80373f2e4815f3d07e1744124e61a82
Darth Vader <yashpvjoshi@gmail.com>
2026-02-03 15:33:26 +0530

    Initial launch sequence

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div>
+                <h1>Orbit Space Club</h1>
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                    <p>
+                        Orbit Space Club - Initial Launch Sequence
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
